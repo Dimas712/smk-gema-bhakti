a092236b9798e244d50946a5e08e36c96bafc57a
Dimas712 <jasinga1933@gmail.com>
2026-06-04 06:40:40 +0700

    perbaikan

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -83,13 +83,6 @@ export const metadata = {
   },
 };
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
