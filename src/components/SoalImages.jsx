a18ddbf7750093c43b201d2e012fe612beb12e27
Dimas712 <jasinga1933@gmail.com>
2026-06-04 06:49:32 +0700

    update perbaikan soal zoom

--- src/components/SoalImages.jsx
+++ src/components/SoalImages.jsx
@@ -1,29 +1,40 @@
 "use client";
 
+import {
+  TransformWrapper,
+  TransformComponent,
+} from "react-zoom-pan-pinch";
+
 export default function SoalImages({
   folder,
   pages,
 }) {
   return (
-    <div
-      className="overflow-y-auto h-full"
-      onContextMenu={(e) =>
-        e.preventDefault()
-      }
+    <TransformWrapper
+      initialScale={1}
+      minScale={1}
+      maxScale={5}
+      wheel={{ disabled: false }}
+      pinch={{ disabled: false }}
+      doubleClick={{ disabled: false }}
     >
-      {Array.from(
-        { length: pages },
-        (_, i) => (
-          <img
-            key={i}
-            src={`/soal/${folder}/${i + 1}.webp`}
-            alt={`Halaman ${i + 1}`}
-            loading="lazy"
-            draggable={false}
-            className="w-full mb-4 select-none"
-          />
-        )
-      )}
-    </div>
+      <TransformComponent>
+        <div>
+          {Array.from(
+            { length: pages },
+            (_, i) => (
+              <img
+                key={i}
+                src={`/soal/${folder}/${i + 1}.webp`}
+                alt={`Halaman ${i + 1}`}
+                loading="lazy"
+                draggable={false}
+                className="w-full mb-4 select-none"
+              />
+            )
+          )}
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
   );
 }
